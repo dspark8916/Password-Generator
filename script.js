33754f857897e1fbb3cbd645c2dee5566c0657f3
Dimitar Micevski <dmicevski@hotmail.com>
2020-10-27 23:09:30 -0400

    Created additional variables & start a function

--- script.js
+++ script.js
@@ -13,7 +13,31 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEDFHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var special = "!@#$%^&*()_+~`|{}[]:;?><,./-=";
+var lowerCase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+var upperCase = ("A", "B", "C", "D", "E", "D", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+var numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+var special = ("!", "@", "#", "$", "%", "&", "*", "(", ")", "+");
+
+// for (var i = 0; i > 8 || i < 128; i++) {
+//   password = password + valuesString.charAt(Math.floor(Math.random) * Math.floor(value.length)); 
+// }
+
+// console.log()
+
+var generate;
+var confirmNumbers;
+var confirmSpecial;
+var confirmUppercase;
+var confirmLowercase;
+
+var choices;
+
+function generatePassword() {
+  generate = pasrseInt(prompt("Your password must be between 8 & 128 characters long. How long you would like your password to be?"));
+    if (!generate) {
+      alert("Please enter a number between 8 & 128.");
+    }
+    else if (generate < 8 || generate > 128) {
+      generate = parseInt(prompt("Please enter a number between 8 & 128."));
+    }
+}
