933f55eb39c8238fde2fbb8733a7cf5a3c5744c7
Dimitar Micevski <dmicevski@hotmail.com>
2020-10-27 20:44:54 -0400

    Created variables for character types

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEDFHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "!@#$%^&*()_+~`|{}[]:;?><,./-=";
