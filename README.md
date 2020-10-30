e41bbc43886857d4f50ff886a050c516f27f4d2f
Dimitar Micevski <dmicevski@hotmail.com>
2020-10-30 16:50:29 -0400

    Created Importance of a strong password section in ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Password-Generator
+
+## The Importance of a Strong Password
+
+    Strong passwords are utterly important – they prevent unauthorized access to your electronic accounts and devices.
+
+    If you choose a very complicated and long password, you will make it very difficult for a hacker to crack it, whether by a brute-force attack (trying every possible combination of numbers, letters or special characters) or an automated machine attack trying thousands of combinations per second to guess your one and only.
+
+    So, the more complex your password is, the more security it provides for your account. Remember that your account is where you store a great deal of sensitive information that you don’t want to have stolen. As you understand, the stakes are very high. Therefore, taking care of your account password is crucial.
+
+    Credit to 
+![Namecheap.com](https://www.namecheap.com/support/knowledgebase/article.aspx/9517/45/what-is-a-secure-password-and-why-is-it-important-to-have-one)
+
--- script.js
+++ script.js
@@ -12,18 +12,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var generator;
+var generator; 
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "D", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "+"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+var upperCase = ["A", "B", "C", "D", "E", "D", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
+var special = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "+"]; 
 
-var verifyNumbers;
-var verifySpecial;
-var verifyUppercase;
-var verifyLowercase;
-var options;
+var verifyNumbers; 
+var verifySpecial; 
+var verifyUppercase; 
+var verifyLowercase; 
+var options; 
 
 function generatePassword() {
     generator = prompt("Your password must be between 8 & 128 characters long. How long you would like your password to be?");
